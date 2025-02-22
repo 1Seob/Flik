6472425f79178cab1622f7f0dcbd229a6aa84d13
1Seob <moon27456@naver.com>
2025-02-23 02:53:56 +0900

    [Feature] 문단 관련 API들 추가

---
+++ src/paragraph/paragrpah.repository.ts
@@ -0,0 +1,55 @@
+import { PrismaService } from 'src/common/services/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { ParagraphData } from './type/paragraph-type';
+
+@Injectable()
+export class ParagraphRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getParagraph(paragraphId: number): Promise<ParagraphData | null> {
+    return this.prisma.paragraph.findUnique({
+      where: {
+        id: paragraphId,
+      },
+    });
+  }
+
+  async toggleParagraphLike(
+    paragraphId: number,
+    userId: number,
+  ): Promise<void> {
+    const like = await this.prisma.paragraphLike.findUnique({
+      where: {
+        userId_paragraphId: {
+          userId,
+          paragraphId,
+        },
+      },
+    });
+    if (like) {
+      await this.prisma.paragraphLike.delete({
+        where: {
+          userId_paragraphId: {
+            userId,
+            paragraphId,
+          },
+        },
+      });
+    } else {
+      await this.prisma.paragraphLike.create({
+        data: {
+          paragraphId,
+          userId,
+        },
+      });
+    }
+  }
+
+  async getParagraphLikeCount(paragraphId: number): Promise<number> {
+    return this.prisma.paragraphLike.count({
+      where: {
+        paragraphId,
+      },
+    });
+  }
+}
